55d65c4346315e4a4abbde46bfdb0ba5fa80d022
zjl <2272465327@qq.com>
2022-03-20 10:44:56 +0800

    netty http协议 入门

--- src/test/java/com/itcast/advance/TestHttp.java
+++ src/test/java/com/itcast/advance/TestHttp.java
@@ -1,7 +1,16 @@
 package com.itcast.advance;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
 
 /**
  * @Description:
@@ -16,6 +25,58 @@ public class TestHttp {
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup worker = new NioEventLoopGroup();
 
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            ChannelFuture channelFuture = serverBootstrap.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        /** Http协议编解码器 - codec */
+                        ch.pipeline().addLast(new HttpServerCodec());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<HttpRequest>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, HttpRequest msg) throws Exception {
+                                log.info("method: {}, uri: {}", msg.method(), msg.uri());
+                                DefaultFullHttpResponse response =
+                                    new DefaultFullHttpResponse(msg.protocolVersion(), HttpResponseStatus.OK);
+                                byte[] bytes = "<h1>hello, world</h1>".getBytes();
+                                //表明响应长度
+                                response.headers().setInt(CONTENT_LENGTH, bytes.length);
+                                //设置响应内容
+                                response.content().writeBytes(bytes);
+
+                                ctx.writeAndFlush(response);
+                            }
+                        });
+//                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+//                            @Override
+//                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//                                log.info("msg class: {}", msg.getClass());
+//                                /** 请求行  请求头 */
+//                                if (msg instanceof HttpRequest) {
+//                                    log.info("HttpRequest msg: {}", msg.toString());
+//                                }
+//                                /** 请求体 -- post请求才会有 body */
+//                                if (msg instanceof HttpContent) {
+//                                    log.info("HttpContent msg: {}", msg.toString());
+//                                }
+//                            }
+//                        });
+                    }
+                })
+                .bind(8080)
+                .sync();
+
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
     }
 
 }
